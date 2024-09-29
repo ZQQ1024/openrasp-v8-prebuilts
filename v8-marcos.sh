d327c9e66e35dd397102f9e07c0a61e902dae119
ZQQ1024 <840486874@qq.com>
2024-09-29 14:00:57 +0800

    Add macos arm64 v8 build

--- v8-marcos.sh
+++ v8-marcos.sh
@@ -2,13 +2,6 @@
 
 VERSION=$1
 
-git config --global user.name "V8 macOS Builder"
-git config --global user.email "v8.macos.builder@localhost"
-git config --global core.autocrlf false
-git config --global core.filemode false
-git config --global color.ui true
-
-
 cd ~
 echo "=====[ Getting Depot Tools ]====="	
 git clone -q https://chromium.googlesource.com/chromium/tools/depot_tools.git
@@ -17,14 +10,14 @@ gclient
 
 echo "=====[ Fetching V8 ]====="
 fetch v8
-echo "target_os = ['mac']" >> .gclient
 cd ~/v8
 git checkout $VERSION
 gclient sync
 
+mkdir -p out/monolith.macos.arm64
 
 echo "=====[ Building V8 ]====="
-python2 ./tools/dev/v8gen.py arm64.release -vv -- '
+cat > out/monolith.macos.arm64/args.gn <<EOF
 target_os = "mac"
 target_cpu = "arm64"
 is_debug = false
@@ -39,13 +32,13 @@ v8_use_snapshot = true
 v8_use_external_startup_data = false
 v8_enable_shared_ro_heap = true
 use_custom_libcxx = false
-'
+EOF
 
-ninja -C out.gn/arm64.release -t clean
-ninja -C out.gn/arm64.release v8
+gn gen out/monolith.macos.arm64
+ninja -C out/monolith.macos.arm64 -j 8 v8_monolith
 
-mkdir -p output/arm64
+mkdir -p output/macos.arm64
 
 cp -r v8/include output/include
 
-cp v8/out/arm64.release/obj/libv8_monolith.a output/arm64/libv8_monolith.a
+cp v8/out/monolith.macos.arm64/obj/libv8_monolith.a output/macos.arm64/libv8_monolith.a
